e9b916c4d550ef4f5bb5468e707d10d994fa0edc
Kimmo Tuokkola <kimmo.tuokkola@accenture.com>
2018-03-12 22:34:38 +0200

    Fix

--- src/main/java/com/foo/login/security/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/com/foo/login/security/service/impl/UserDetailsServiceImpl.java
@@ -23,6 +23,16 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 	private UserRepository userRepository;
  
  
+	/**
+	 * In order to have authentication by a DAO class, we need to implement the UserDetailsService interface. 
+	 * This class is used to load user information when the user uses form login- loadUserByUsername() method 
+	 * which is used to validate the user. The implementation is handling:
+	 * - find username in DB, if not found throw an exception
+     * - if the user is found, login user and return the User object of type org.springframework.security.core.userdetails.User, 
+     *   spring will automatically update the Security context for us. 
+     *   
+     * Note the com.foo.login.security.SecurityConfig.configureGlobal(AuthenticationManagerBuilder)
+	 */
 	@Override
 	@Transactional(readOnly = true)
 	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
